578b72f718ed4fc8f6230344f4f88bec3876f791
allivan2020 <allivan2020@gmail.com>
2025-12-19 18:20:03 +0200

    home work 9

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,1 +1,44 @@
-console.log('form');
+const formData = { email: '', message: '' };
+
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', onFormInput);
+
+function onFormInput(event) {
+  const name = event.target.name;
+  const value = event.target.value.trim();
+
+  formData[name] = value;
+
+  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+}
+
+const savedData = localStorage.getItem('feedback-form-state');
+
+if (savedData) {
+  const parsedData = JSON.parse(savedData);
+  form.elements.email.value = parsedData.email || '';
+  form.elements.message.value = parsedData.message || '';
+
+  formData.email = parsedData.email || '';
+  formData.message = parsedData.message || '';
+}
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  if (!formData.email || !formData.message) {
+    alert('Fill please all fields');
+    return;
+  }
+
+  console.log(formData);
+
+  form.reset();
+
+  localStorage.removeItem('feedback-form-state');
+
+  formData.email = '';
+  formData.message = '';
+}
